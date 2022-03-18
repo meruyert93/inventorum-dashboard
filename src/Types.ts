39fce846075f738d5e1b2a50533c146da2de61fe
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2022-03-18 17:41:14 +0100

    feat: add static types

---
+++ src/Types.ts
@@ -0,0 +1,18 @@
+export type MenuItem =
+  | "Home"
+  | "Product Management"
+  | "Contacts"
+  | "Orders"
+  | "Reports"
+  | "API & Apps"
+  | "Integrations";
+
+export const MenuList: MenuItem[] = [
+  "Home",
+  "Product Management",
+  "Contacts",
+  "Orders",
+  "Reports",
+  "API & Apps",
+  "Integrations",
+];
